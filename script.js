5b6881950fe270fc1f4696fd698d0ca771d83bb8
camilo-08-dev <davidlealnarvaez1@gmail.com>
2020-09-27 21:25:12 -0500

    Artistas js

---
+++ script.js
@@ -0,0 +1,59 @@
+function Ruben(){
+
+    let name = prompt("¿Quien es ese artista?")
+    let nombre = document.getElementById("nombre")
+    nombre.textContent = name
+
+    let genero = prompt("¿A que genero musical pertenece el artista?")
+    let generom = document.getElementById("generom")
+    generom.textContent = genero
+
+    let country = prompt("¿De que pais es el artista?")
+    let pais = document.getElementById("pais")
+    pais.textContent = country
+
+    let song = prompt("Escribe una cancion que represente este artista")
+    let cancion = document.getElementById("cancion")
+   cancion.textContent = song
+}
+
+function Daddy(){
+
+let name = prompt("¿Quien es ese artista?")
+    let nombre1 = document.getElementById("nombre1")
+    nombre1.textContent = name
+
+    let genero = prompt("¿A que genero musical pertenece el artista?")
+    let generom1 = document.getElementById("generom1")
+    generom1.textContent = genero
+
+    let country = prompt("¿De que pais es el artista?")
+    let pais1 = document.getElementById("pais1")
+    pais1.textContent = country
+
+    let song = prompt("Escribe una cancion que represente este artista")
+    let cancion1 = document.getElementById("cancion1")
+   cancion1.textContent = song
+}
+
+function Sean(){
+
+    let name = prompt("¿Quien es ese artista?")
+    let nombre2 = document.getElementById("nombre2")
+    nombre2.textContent = name
+
+    let genero = prompt("¿A que genero musical pertenece el artista?")
+    let generom2 = document.getElementById("generom2")
+    generom2.textContent = genero
+
+    let country = prompt("¿De que pais es el artista?")
+    let pais2 = document.getElementById("pais2")
+    pais2.textContent = country
+
+    let song = prompt("Escribe una cancion que represente este artista")
+    let cancion2 = document.getElementById("cancion2")
+   cancion2.textContent = song
+
+
+
+}
